f44ef5fd52724936189989044189990b34155811
SupenBysz <supen.huang@qq.com>
2025-04-07 04:42:00 +0800

    修复(Common): 更新 API 调用以简化响应类型
    - 将多个 API 调用的返回类型从 ApiResponse<T> 更改为直接的类型 T，以简化代码结构
    - 更新 getFileById 方法，增加 token 参数以支持更灵活的文件获取
    - 调整 getFileUrl 方法，接受更复杂的参数对象以增强功能

--- src/api/common/index.ts
+++ src/api/common/index.ts
@@ -5,17 +5,17 @@ import { ApiResponse } from '@kysion/utils';
 export class Common {
     // 发送短信验证码
     public static sendCaptchaBySms({ captchaType, mobile, scene }: { captchaType: CaptchaTypeEnum; mobile: string; scene: string }) {
-        return http.post<ApiResponse<{ captchaId: string }>>('/common/sendCaptchaBySms', { captchaType, mobile, scene });
+        return http.post<{ captchaId: string }>('/common/sendCaptchaBySms', { captchaType, mobile, scene });
     }
 
     // 发送邮件验证码
     public static sendCaptchaByMail({ captchaType, mail, scene }: { captchaType: CaptchaTypeEnum; mail: string; scene: string }) {
-        return http.post<ApiResponse<{ captchaId: string }>>('/common/sendCaptchaByMail', { captchaType, mail, scene });
+        return http.post<{ captchaId: string }>('/common/sendCaptchaByMail', { captchaType, mail, scene });
     }
 
     // 获取验证码
     public static captcha() {
-        return http.get<ApiResponse<{ captchaId: string; captchaImg: string }>>(`/common/captcha`);
+        return http.get<{ captchaId: string; captchaImg: string }>(`/common/captcha`);
     }
 
     /**
@@ -29,7 +29,7 @@ export class Common {
         if (params.type) {
             formData.append('type', params.type);
         }
-        return http.post<ApiResponse<{ id: number | string; url: string }>>(`/common/file/upload`, formData, {
+        return http.post<{ id: number | string; url: string }>(`/common/file/upload`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             } as any,
@@ -48,7 +48,7 @@ export class Common {
             formData.append('type', params.type);
         }
 
-        return http.post<ApiResponse<{ id: number | string; url: string }>>(`/common/file/uploadPicture`, formData, {
+        return http.post<{ id: number | string; url: string }>(`/common/file/uploadPicture`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             } as any,
@@ -60,8 +60,8 @@ export class Common {
      * @param params 文件ID参数
      * @returns Promise
      */
-    public static getFileById(params: { id: number | string }) {
-        return http.post<ApiResponse<{ id: number | string; url: string; name: string; size: number; type: string }>>(`/common/file/getFileById`, params);
+    public static getFileById(params: { id: number | string, token: string }) {
+        return http.get<{ id: number | string; url: string; name: string; size: number; type: string }>(`/common/file/getFileById`, params);
     }
 
     /**
@@ -69,8 +69,8 @@ export class Common {
      * @param id 文件ID
      * @returns 文件URL
      */
-    public static getFileUrl(id: number | string) {
-        return `/common/getFile?id=${id}`;
+    public static getFileUrl(params: { id?: number | string, path?: string, cid?: number | string, styleStr?: string, token: string }) {
+        return `/common/file/getFileById?id=${params.id}&path=${params.path ?? ''}&cid=${params.cid ?? ''}&styleStr=${params.styleStr ?? ''}&token=${params.token}`;
     }
 
     /**
@@ -79,7 +79,7 @@ export class Common {
      * @returns Promise
      */
     public static getAreaListByLevel(params: { level: number }) {
-        return http.post<ApiResponse<any[]>>(`/common/area/getAreaListByLevel`, params);
+        return http.post<any[]>(`/common/area/getAreaListByLevel`, params);
     }
 
     /**
@@ -88,7 +88,7 @@ export class Common {
      * @returns Promise
      */
     public static getAreaListByParentId(params: { parentId: number | string }) {
-        return http.post<ApiResponse<any[]>>(`/common/area/getAreaListByParentId`, params);
+        return http.post<any[]>(`/common/area/getAreaListByParentId`, params);
     }
 
     /**
@@ -100,7 +100,7 @@ export class Common {
         const formData = new FormData();
         formData.append('file', params.file);
 
-        return http.post<ApiResponse<any>>(`/common/file/uploadIDCardWithOCR`, formData, {
+        return http.post<any>(`/common/file/uploadIDCardWithOCR`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             } as any,
@@ -116,7 +116,7 @@ export class Common {
         const formData = new FormData();
         formData.append('file', params.file);
 
-        return http.post<ApiResponse<any>>(`/common/file/uploadBankCardWithOCR`, formData, {
+        return http.post<any>(`/common/file/uploadBankCardWithOCR`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             } as any,
@@ -132,7 +132,7 @@ export class Common {
         const formData = new FormData();
         formData.append('file', params.file);
 
-        return http.post<ApiResponse<any>>(`/common/file/uploadBusinessLicenseWithOCR`, formData, {
+        return http.post<any>(`/common/file/uploadBusinessLicenseWithOCR`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             } as any,
